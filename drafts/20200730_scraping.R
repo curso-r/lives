9c49b09388ccf7db9b0559271ceaf526b90158bd
C. Lente <7017340+clente@users.noreply.github.com>
2020-07-30 21:59:08 -0300

    Mais um scraper

--- drafts/20200730_scraping.R
+++ drafts/20200730_scraping.R
@@ -30,3 +30,51 @@ df <- df %>%
   dplyr::left_join(arqs)
 
 readr::write_rds(df, "~/Documents/lives/data-raw/df_contratos_covid.rds")
+
+
+
+get_drug <- function(drug) {
+  url <- "https://api.who-umc.org/vigibase/icsrstatistics/dimensions/drug"
+  query <- list(tradename = drug)
+
+  headers <- httr::add_headers(
+    "Authorization" = "Basic VmlnaUJhc2VBY2Nlc3NDbGllbnQ6cHN3ZDRWaUE=",
+    "umc-client-key" = "6d851d41-f558-4805-a9a6-08df0e0e414b"
+  )
+
+  info <- url %>%
+    httr::GET(query = query, httr::accept_json(), headers) %>%
+    xml2::read_html() %>%
+    xml2::xml_find_first("//p") %>%
+    xml2::xml_text() %>%
+    jsonlite::fromJSON()
+
+  url <- "https://api.who-umc.org/vigibase/icsrstatistics/distributions"
+  query <- list(
+    agegroupFilter = "",
+    continentFilter = "",
+    reactionFilter = "",
+    sexFilter = "",
+    substanceFilter = info[[1]]
+  )
+
+  url %>%
+    httr::GET(query = query, httr::accept_json(), headers) %>%
+    xml2::read_html() %>%
+    xml2::xml_find_first("//p") %>%
+    xml2::xml_text() %>%
+    jsonlite::fromJSON() %>%
+    utils::tail(-1) %>%
+    purrr::map(dplyr::as_tibble) %>%
+    purrr::map(janitor::clean_names) %>%
+    purrr::map(list) %>%
+    dplyr::as_tibble() %>%
+    janitor::clean_names() %>%
+    dplyr::mutate(
+      tradename = drug,
+      substrance_id = info[[1]]
+    ) %>%
+    dplyr::relocate(substrance_id, tradename)
+}
+
+dplyr::glimpse(get_drug("tylenol"))
