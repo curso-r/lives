b2b8f996b1096d6a5454614fa85d4f954489d94f
Julio Trecenti <julio.trecenti@gmail.com>
2020-08-13 20:54:09 -0300

    pesqele e shinydisconnect

---
+++ drafts/20200813_shinydisconnect.R
@@ -0,0 +1,36 @@
+#' Author: Julio Trecenti
+#' Subject:
+
+# library(tidyverse)
+library(magrittr)
+
+install.packages("shinydisconnect")
+
+?shinydisconnect::disconnectMessage()
+
+
+library(shiny)
+library(shinydisconnect)
+shinyApp(
+  ui = fluidPage(
+    disconnectMessage(),
+    actionButton("disconnect", "Disconnect the app")
+  ),
+  server = function(input, output, session) {
+    observeEvent(input$disconnect, {
+      session$close()
+    })
+  }
+)
+
+# Import -----------------------------------------------------------------------
+
+# Tidy -------------------------------------------------------------------------
+
+# Visualize --------------------------------------------------------------------
+
+# Model ------------------------------------------------------------------------
+
+# Export -----------------------------------------------------------------------
+
+# readr::write_rds(d, "")
