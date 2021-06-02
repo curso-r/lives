c4342b195e0a6e47e08843999432e6832ef909da
Fernando Corrêa <fptcorrea@gmail.com>
2021-06-02 18:41:18 -0300

    incluindo mais pacotes

--- drafts/20210602_extensoes_ggplot2.R
+++ drafts/20210602_extensoes_ggplot2.R
@@ -9,7 +9,7 @@
 # mas são imports do pacote
 
 
-# conexões---- ----------------------------------------------------------------
+# conexões--------------------------------------------------------------------
 
 library(tidyverse)
 library(bigrquery)
@@ -39,7 +39,10 @@ conexao_populacao <- dbConnect(
 
 escola <- tbl(conexao_ideb, "escola") %>%
   group_by(ano, estado_abrev, municipio) %>%
-  summarise(ideb = mean(ideb, na.rm = TRUE)) %>%
+  summarise(ideb = mean(ideb, na.rm = TRUE),
+            saeb_matematica = mean(nota_saeb_matematica, na.rm = TRUE),
+            saeb_portugues = mean(nota_saeb_lingua_portuguesa, na.rm = TRUE)
+            ) %>%
   ungroup() %>%
   collect()
 
@@ -47,8 +50,6 @@ escola %>%
   mutate(
     estado_abrev = fct_reorder(estado_abrev, ideb, .fun = median, na.rm = TRUE)
   ) %>%
-  #filter(ano %in% c("2005", "2019")) %>%
-  #mutate(ano = as_factor(ano)) %>%
   ggplot(aes(x = ideb, y = estado_abrev, fill = estado_abrev)) +
   ggridges::geom_density_ridges(color = 'transparent', alpha = .6) +
   scale_fill_viridis_d() +
@@ -61,8 +62,9 @@ escola %>%
     legend.position = 'none') +
   geom_vline(xintercept = 4)
 
-
 # gganimate ---------------------------------------------------------------
+
+# https://ugoproto.github.io/ugo_r_doc/pdf/gganimate.pdf
 
 vacinacao_base <- tbl(conexao_covid, "microdados_vacinacao") %>%
   count(sigla_uf, data_aplicacao, dose) %>%
@@ -111,13 +113,7 @@ populacao_estados <- tbl(conexao_populacao, "municipios") %>%
     populacao = sum(populacao)
   )
 
-populacao_regiao <- vacinacao_base_2 %>%
-  group_by(regiao) %>%
-  summarise(
-    populacao = sum(unique(populacao))
-  )
-
-vacinacao_base_2 <- vacinacao_base %>%
+vacinacao_base_por_estado <- vacinacao_base %>%
   ungroup() %>%
   mutate(
     regiao = case_when(
@@ -140,6 +136,12 @@ vacinacao_base_2 <- vacinacao_base %>%
   ) %>%
   mutate(
     percentual_vacinado = n_acum/populacao
+  )
+
+populacao_regiao <- vacinacao_base_por_estado %>%
+  group_by(regiao) %>%
+  summarise(
+    populacao = sum(unique(populacao))
   )
 
 vacinacao_base_por_regiao <-  vacinacao_base %>%
@@ -173,7 +175,7 @@ vacinacao_base_por_regiao <-  vacinacao_base %>%
 
 library(gganimate)
 
-grafico_por_estado <- vacinacao_base_2 %>%
+grafico_por_estado <- vacinacao_base_por_estado %>%
   ungroup() %>%
   arrange(data_aplicacao) %>%
   #mutate(data_aplicacao = as.numeric(data_aplicacao)) %>%
@@ -185,20 +187,170 @@ grafico_por_estado <- vacinacao_base_2 %>%
   theme_bw()
 
 grafico_por_regiao <- vacinacao_base_por_regiao %>%
+  #filter(regiao == "Sudeste") %>%
+  #filter(data_aplicacao <= as.Date("2021-04-15")) %>%
   ungroup() %>%
   arrange(data_aplicacao) %>%
   #mutate(data_aplicacao = as.numeric(data_aplicacao)) %>%
   ggplot(aes(x = data_aplicacao, y = percentual_vacinado, color = regiao)) +
   #geom_col() +
-  geom_line(size = 1.2) +
-  #geom_point() +
+  #geom_line(size = 1.2) +
+  geom_point(size = 6) +
   transition_reveal(data_aplicacao) +
   scale_color_viridis_d() +
-  theme_bw()
-
-# https://ugoproto.github.io/ugo_r_doc/pdf/gganimate.pdf
+  theme_bw(20) +
+  shadow_wake(wake_length = 0.5, size = 3) +
+  ease_aes("cubic-in") +
+  labs(x = "Data de referência", "% da população vacinado com a 2a dose")
 
 animate(grafico_por_regiao, height = 600, width =800)
 
-anim_save("animacao.gif", animation = grafico)
+anim_save("animacao.gif", animation = grafico_por_regiao, height = 600, width =800)
+
+
+# patchwork + grid --------------------------------------------------------
+
+library(patchwork)
+library(ggplot2)
+library(magrittr)
+
+# primeiro grafico
+g1 <- dados::pinguins %>%
+  ggplot(aes(x = comprimento_bico, y = profundidade_bico)) +
+  geom_point()
+
+# segundo grafico
+g2 <- dados::pinguins %>%
+  ggplot(aes(x = profundidade_bico)) +
+  geom_density()
+
+# terceiro grafico
+g3 <- dados::pinguins %>%
+  ggplot(aes(x = comprimento_bico)) +
+  geom_density()
+
+# codigo usando o pacote patchwork
+# aqui usamos esse operador '+' (que também poderia ser `|`), que fica disponível
+# quando carregamos o pacote
+g1 + g2 + g3
+
+# codigo usando o pacote patchwork
+# aqui usamos esse operador '/', que fica disponível
+# quando carregamos o pacote
+g1 / g2 / g3
+
+# codigo usando o pacote patchwork
+# aqui usamos esse operador '/', que fica disponível
+# quando carregamos o pacote
+(g1 + g2) / g3
+
+# exemplo mais maluco, podemos misturar quantos gráficos a gente quiser e do jeito que a gente quiser:
+(g1+((g1 + g2) / g3))/g3
+
+# Exemplo com gráfico
+# primeira linha
+(g1 + grid::textGrob("Todos os gráficos indicam que está\nrolando uma parad bi-modal."))/
+  # segunda linha
+  (g2+g3)
+
+tabela <- dados::pinguins %>%
+  dplyr::summarise(
+    comprimento_bico = mean(comprimento_bico, na.rm = TRUE),
+    profundidade_bico = mean(profundidade_bico, na.rm = TRUE)
+  ) %>%
+  tidyr::pivot_longer(dplyr::everything(), names_to = "Variável", values_to = "Média")
+
+# Exemplo com tabela
+# primeira linha
+(g1 + gridExtra::tableGrob(tabela, rows = c("", "")))/
+  # segunda linha
+  (g2+g3)
+
+((
+  # gráfico
+  (# primeira coluna
+    (g2 / g3) |
+      # segunda coluna
+      g1) +
+    # alterando a largura
+    plot_layout(widths = c(1, 2)
+    )
+) +
+    #
+    plot_annotation(
+      title = "Comparação entre os a profundidade e o comprimento dos bicos",
+      subtitle = "Os  graficos indicam a presença de uma distribuição bimodal, possivelmente porque existem várias espécies na base.",
+      caption = "Fonte: github.com/cienciadedatos/dados"
+    )) &
+  # trocando os temas para branco. o operador `&` garante que o tema será aplicado
+  # a todos os sub-plots
+  theme_bw()
+
+# ggfortify ---------------------------------------------------------------
+
+# https://github.com/sinhrks/ggfortify
+
+install.packages("ggfortify")
+
+library(ggfortify)
+
+ctl <- c(4.17,5.58,5.18,6.11,4.50,4.61,5.17,4.53,5.33,5.14)
+trt <- c(4.81,4.17,4.41,3.59,5.87,3.83,6.03,4.89,4.32,4.69)
+group <- gl(2, 10, 20, labels = c("Ctl","Trt"))
+weight <- c(ctl, trt)
+lm.D9 <- lm(weight ~ group)
+
+# Exemplo modelo linear
+autoplot(lm.D9)
+
+# Exemplo ACF
+autoplot(acf(lh))
+
+library(survival)
+
+autoplot(survfit( Surv(futime, fustat)~1, data=ovarian))
+
+# especificamente sobre modelos de sobrevivencia, tambem existe o survminer
+
+# gghighlight -------------------------------------------------------------
+
+diamonds %>%
+  ggplot(aes(x = carat, y = price)) +
+  geom_point() +
+  gghighlight::gghighlight(carat > 4, label_key = carat) +
+  cowplot::theme_minimal_grid()
+
+# ggrepel -----------------------------------------------------------------
+
+library(ggrepel)
+
+escola %>%
+  filter(ano == "2019", estado_abrev == "SP") %>%
+  ggplot(aes(x = saeb_matematica, y = saeb_portugues, label = municipio)) +
+  geom_point() +
+  # feio
+  #geom_text() +
+  # bonito
+  geom_text_repel() +
+  theme_minimal(15)
+
+# esquisse ----------------------------------------------------------------
+
+esquisse::esquisser()
+# or with your data:
+esquisse::esquisser(dados::pinguins)
+
+# ggedit ------------------------------------------------------------------
+
+library('ggedit')
+library(ggplot2)
+p <- ggplot(mtcars, aes(x = hp, y = wt)) +
+  geom_point() +
+  geom_smooth() +
+  theme_minimal()
+
+p2 <- ggedit(p)
+
+names(p2) # will show you which objects are available.
+plot(p2)
 
