a38c8a6d99c9fd146e2ed681cc59fecac6981860
jtrecenti <julio.trecenti@gmail.com>
2021-06-09 20:59:56 -0300

    web scraping sefaz

---
+++ drafts/20210609_webscraping_sefaz.R
@@ -0,0 +1,102 @@
+#' Author:
+#' Subject:
+
+# library(tidyverse)
+library(magrittr)
+
+# acesso
+
+u <- "`http://internet-consultapublica.apps.sefaz.ce.gov.br/contencioso/consultar`"
+
+body <- list(
+  "numano" = "",
+  "descricaocamara" = "3"
+)
+
+r <- httr::POST(u, body = body, encode = "form")
+
+# scraping
+
+tag_tabela <- r %>%
+  xml2::read_html() %>%
+  xml2::xml_find_first("//table[@id='resolucoes']")
+
+links <- tag_tabela %>%
+  xml2::xml_find_all(".//a") %>%
+  xml2::xml_attr("href")
+
+da_tabela <- tag_tabela %>%
+  rvest::html_table() %>%
+  tibble::as_tibble() %>%
+  dplyr::mutate(link = links) %>%
+  janitor::clean_names()
+
+baixa_camara <- function(camara) {
+  u <- "http://internet-consultapublica.apps.sefaz.ce.gov.br/contencioso/consultar"
+
+  body <- list(
+    "numano" = "",
+    "descricaocamara" = camara
+  )
+
+  r <- httr::POST(u, body = body, encode = "form")
+
+  tag_tabela <- r %>%
+    xml2::read_html() %>%
+    xml2::xml_find_first("//table[@id='resolucoes']")
+
+  links <- tag_tabela %>%
+    xml2::xml_find_all(".//a") %>%
+    xml2::xml_attr("href")
+
+  da_tabela <- tag_tabela %>%
+    rvest::html_table() %>%
+    tibble::as_tibble() %>%
+    dplyr::mutate(link = links) %>%
+    janitor::clean_names()
+
+
+  da_tabela
+}
+
+dados_resultado <- purrr::map_dfr(
+  camaras,
+  baixa_camara,
+  .id = "id_camara"
+)
+
+tag_camaras <- "http://internet-consultapublica.apps.sefaz.ce.gov.br/contencioso/preparar-consultar?numano=2015&descricaocamara=" %>%
+  httr::GET() %>%
+  xml2::read_html() %>%
+  xml2::xml_find_all("//*[@id='descricaocamara']/option")
+
+ids_camaras <- tag_camaras %>%
+  xml2::xml_attr("value")
+
+nomes_camaras <- tag_camaras %>%
+  xml2::xml_text()
+
+tab_camaras <- tibble::tibble(
+  id_camara = ids_camaras,
+  nm_camara = nomes_camaras
+) %>%
+  dplyr::filter(id_camara != "")
+
+da_sefaz <- dados_resultado %>%
+  dplyr::inner_join(tab_camaras, c("id_camara")) %>%
+  dplyr::select(-acoes) %>%
+  dplyr::mutate(
+    ementas = stringr::str_squish(ementas),
+    ementas = abjutils::rm_accent(ementas)
+  )
+
+
+# Tidy -------------------------------------------------------------------------
+
+# Visualize --------------------------------------------------------------------
+
+# Model ------------------------------------------------------------------------
+
+# Export -----------------------------------------------------------------------
+
+# readr::write_rds(d, "")
