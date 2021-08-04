1e48f33011dcd1b0652f6fbbb39c82efd6108a17
williamorim <thewilliam89@gmail.com>
2021-08-04 19:40:21 -0300

    arrumando app pra live

--- drafts/20210804-shiny-rmarkdown/rascunho/app.R
+++ drafts/20210804-shiny-rmarkdown/rascunho/app.R
@@ -1,12 +1,13 @@
 library(shiny)
+library(ggplot2)
 library(dplyr)
 
-imdb <- basesCursoR::pegar_base("imdb") |>
+imdb <- basesCursoR::pegar_base("imdb") %>%
   tidyr::drop_na(data_lancamento, nota_imdb)
 
-# pessoas <- stringr::str_split(c(imdb$elenco, imdb$direcao), pattern = ", ") |>
-#   purrr::flatten_chr() |>
-#   unique() |>
+# pessoas <- stringr::str_split(c(imdb$elenco, imdb$direcao), pattern = ", ") %>%
+#   purrr::flatten_chr() %>%
+#   unique() %>%
 #   sort()
 
 ui <- fluidPage(
@@ -33,29 +34,29 @@ server <- function(input, output, session) {
 
   output$grafico <- renderPlot({
 
-    filmes_da_pessoa <- basesCursoR::pegar_base("imdb") |>
+    filmes_da_pessoa <- basesCursoR::pegar_base("imdb") %>%
       mutate(
         data_lancamento = as.Date(data_lancamento)
-      ) |>
+      ) %>%
       filter(
         stringr::str_detect(direcao, input$pessoa) |
           stringr::str_detect(elenco, input$pessoa)
       )
 
     nota_dos_melhores_filmes <- quantile(filmes_da_pessoa$nota_imdb, .9)
-    tab_melhores <- filmes_da_pessoa |>
-      filter(nota_imdb >= nota_dos_melhores_filmes) |>
-      select(data_lancamento, titulo_original, nota_imdb) |>
+    tab_melhores <- filmes_da_pessoa %>%
+      filter(nota_imdb >= nota_dos_melhores_filmes) %>%
+      select(data_lancamento, titulo_original, nota_imdb) %>%
       arrange(desc(nota_imdb))
 
     nota_dos_piores_filmes <- quantile(filmes_da_pessoa$nota_imdb, .1)
-    tab_piores <- filmes_da_pessoa |>
-      filter(nota_imdb <= nota_dos_piores_filmes) |>
-      select(data_lancamento, titulo_original, nota_imdb, num_avaliacoes) |>
+    tab_piores <- filmes_da_pessoa %>%
+      filter(nota_imdb <= nota_dos_piores_filmes) %>%
+      select(data_lancamento, titulo_original, nota_imdb, num_avaliacoes) %>%
       arrange(desc(nota_imdb))
 
-    filmes_da_pessoa |>
-      filter(nota_imdb > nota_dos_piores_filmes, nota_imdb < nota_dos_melhores_filmes) |>
+    filmes_da_pessoa %>%
+      filter(nota_imdb > nota_dos_piores_filmes, nota_imdb < nota_dos_melhores_filmes) %>%
       ggplot(aes(x = data_lancamento, y = nota_imdb)) +
       geom_point(color = "grey") +
       geom_smooth(se = FALSE, color = "grey") +
