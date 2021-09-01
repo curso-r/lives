fb8e7a483f2333fe8c03c489c41d057bc53efc9f
Fernando Corrêa <fptcorrea@gmail.com>
2021-09-01 20:13:33 -0300

    time series

---
+++ drafts/20210901_time_series.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+library(bigrquery)
+library(tidymodels)
+library(modeltime)
+
+conexao_covid <- dbConnect(
+  bigrquery::bigquery(),
+  project = "basedosdados",
+  dataset = "br_ms_vacinacao_covid19",
+  billing = "live-curso-r-bd"
+)
+
+vacinacao_base <- tbl(conexao_covid, "microdados_vacinacao") %>%
+  filter(sigla_uf == "SP") %>%
+  count(sigla_uf, data_aplicacao, dose) %>%
+  collect() %>%
+  mutate(
+    data_aplicacao = as.Date(data_aplicacao)
+  )
+
+segunda_dose <- vacinacao_base %>%
+  filter(dose == "2a Dose",
+         data_aplicacao > as.Date("2021-03-01")) %>%
+  arrange(data_aplicacao)
+
+splits <- initial_time_split(segunda_dose, prop = .8)
+
+model_spec <- arima_reg() %>%
+  set_engine("auto_arima")
+
+model_fit <- model_spec %>%
+  fit(log(n) ~ data_aplicacao, data = training(splits))
+
+testing(splits) %>%
+  add_column(
+    n_predito = exp(predict(model_fit, new_data = testing(splits))$.pred)) %>%
+  pivot_longer(c(n, n_predito), names_to = "tipo", values_to = "valor") %>%
+  bind_rows(training(splits) %>% mutate(tipo = "n", valor = n)) %>%
+  ggplot(aes(x = data_aplicacao, y = valor, color = tipo)) +
+  geom_line()
+
