54f3a04a7fcb5231880214bf93a080118b7ddd36
jtrecenti <julio.trecenti@gmail.com>
2022-04-27 20:07:23 -0300

    mais um gg

---
+++ drafts/20220316_ggplot_qq_coisa.R
@@ -0,0 +1,17 @@
+#' Author: Julio Trecenti
+#' Subject: Reproduzindo ggplot
+#' link: https://raw.githubusercontent.com/adamribaudo/storytelling-with-data-ggplot/master/plot%20output/FIG0227.png
+
+library(tidyverse)
+
+# Import -----------------------------------------------------------------------
+
+dplyr::min_rank()
+
+# Visualize --------------------------------------------------------------------
+
+# Model ------------------------------------------------------------------------
+
+# Export -----------------------------------------------------------------------
+
+# readr::write_rds(d, "")
